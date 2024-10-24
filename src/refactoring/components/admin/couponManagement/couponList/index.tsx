3e38e1230466124bd55f984ba30cf4aba0836129
JANG DONG JIN <jdwls27@naver.com>
2024-10-24 18:37:45 +0900

    couponList

---
+++ src/refactoring/components/admin/couponManagement/couponList/index.tsx
@@ -0,0 +1,21 @@
+import { useCoupons } from '../../../../hooks';
+import Title from '../../../../title/Title';
+
+const index = () => {
+  const { coupons } = useCoupons();
+  return (
+    <div>
+      <Title level={3} text="현재 쿠폰 목록" size={'lg'} />
+      <div className="space-y-2">
+        {coupons.map((coupon, index) => (
+          <div key={index} data-testid={`coupon-${index + 1}`} className="bg-gray-100 p-2 rounded">
+            {coupon.name} ({coupon.code}):
+            {coupon.discountType === 'amount' ? `${coupon.discountValue}원` : `${coupon.discountValue}%`} 할인
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default index;
