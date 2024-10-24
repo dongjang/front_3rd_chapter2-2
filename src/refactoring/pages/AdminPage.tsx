1b6a6fecfd8df954000de6304f974a4231086d7b
JANG DONG JIN <jdwls27@naver.com>
2024-10-24 18:34:43 +0900

    admin,cartpage 수정

---
+++ src/refactoring/pages/AdminPage.tsx
@@ -0,0 +1,21 @@
+import CouponManagement from '../components/admin/couponManagement/index.tsx';
+import ProductManagement from '../components/admin/productManagement/index.tsx';
+import Title from '../title/Title.tsx';
+
+export const AdminPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Title level={1} text={'관리자 페이지'} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <Title level={2} mbNum={4} text="상품 관리" />
+          <ProductManagement />
+        </div>
+        <div>
+          <Title level={2} mbNum={4} text="쿠폰 관리" />
+          <CouponManagement />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/refactoring/pages/CartPage.tsx
@@ -0,0 +1,20 @@
+import CartProductList from '../components/cart/cartProductList/index.tsx';
+import CartList from '../components/cart/cartList/index.tsx';
+import Title from '../title/Title.tsx';
+
+export const CartPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Title level={1} text={'장바구니'} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <CartProductList />
+        </div>
+        <div>
+          <Title level={2} mbNum={4} text={'장바구니 내역'} />
+          <CartList />
+        </div>
+      </div>
+    </div>
+  );
+};
