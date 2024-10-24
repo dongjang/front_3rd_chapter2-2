0d9731432988b598158037551cf97dbea60e8501
JANG DONG JIN <jdwls27@naver.com>
2024-10-24 18:37:00 +0900

    productList

---
+++ src/refactoring/components/admin/productManagement/productManagementList/index.tsx
@@ -0,0 +1,25 @@
+import { useProducts } from '../../../../hooks';
+import ProductManagementEdit from './productManagementEdit';
+
+const index = () => {
+  const { products, openProductIds, handleAccordionToggle } = useProducts();
+  return (
+    <div>
+      <div className="space-y-2">
+        {products.map((product, index) => (
+          <div key={product.id} data-testid={`product-${index + 1}`} className="bg-white p-4 rounded shadow">
+            <button
+              data-testid="toggle-button"
+              onClick={() => handleAccordionToggle(product)}
+              className="w-full text-left font-semibold">
+              {product.name} - {product.price}원 (재고: {product.stock})
+            </button>
+            {openProductIds.get(product.id) && <ProductManagementEdit productId={product.id} />}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default index;
